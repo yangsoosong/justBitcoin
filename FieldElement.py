a1ec3b0dfe89575eb52c7c06e42f527d3ea827b4
Yangsoo Song <song.yang@husky.neu.edu>
2020-03-22 22:42:07 -0400

    Add ne and multiply method for FieldElement class

--- FieldElement.py
+++ FieldElement.py
@@ -13,10 +13,23 @@ class FieldElement:
         if other is None:
             return False
         return self.num == other.num and self.prime == other.prime
-    
+
+    def __ne__(self, other):
+        # this should be the inverse of the == operator
+        return not (self == other)
+
     def __add__(self, other):
         if self.prime != other.prime:
             raise TypeError('Cannot add two numbers in different Fields')
         num = (self.num + other.num) % self.prime
         return self.__class__(num, self.prime)
 
+    def __sub__(self, other):
+        if self.prime != other.prime:
+            raise TypeError('Cannot subtract two numbers in different Fields')
+        # self.num and other.num are the actual values
+        # self.prime is what we need to mod against
+        # We return an element of the same class
+        num = (self.num - other.num) % self.prime
+        return self.__class__(num, self.prime)
+#         raise NotImplementedError
