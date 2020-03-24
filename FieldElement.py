e95e42676e2e9898f31b2e443844dceef77e1f57
Yangsoo Song <song.yang@husky.neu.edu>
2020-03-23 23:35:40 -0400

    Add truediv function

--- FieldElement.py
+++ FieldElement.py
@@ -49,6 +49,17 @@ class FieldElement:
         num = (self.num ** exponent) % self.prime
         return self.__class__(num, self.prime)
 
+    def __truediv__(self, other):
+        if self.prime != other.prime:
+            raise TypeError('Cannot divide two numbers in different fields')
+        # use Fermat's little theorem:
+        # self.num**(p-1) % p == 1
+        # this means:
+        # 1/n == pow(n, p-2, p)
+        # we return an element of the same class
+        num = self.num * pow(other.num, self.prime - 2, self.prime) % self.prime 
+        return self.__class__(num, self.prime)
+
 
 class FieldElementTest(TestCase):
 
